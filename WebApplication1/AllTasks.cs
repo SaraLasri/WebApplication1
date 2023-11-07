4ec293f8c7e331f5a1015b7e697a0787b95ce765
faigygit <37561388215@mby.co.il>
2023-11-07 17:09:48 +0200

    new change

--- WebApplication1/AllTasks.cs
+++ WebApplication1/AllTasks.cs
@@ -4,7 +4,7 @@
     {
         public static List<TaskModel> allTasks = new List<TaskModel>{
 
-        new TaskModel(1, "fff","Orange", DateTime.Now,Preferance.a),
+        new TaskModel(1, "fff","Yellow", DateTime.Now,Preferance.a),
         new TaskModel(2,"ggg","Orange", DateTime.Now,Preferance.a),
         new TaskModel(3, "fff","Green", DateTime.Now,Preferance.a),
         new TaskModel(4, "uuu","Orange", DateTime.Now,Preferance.a)
