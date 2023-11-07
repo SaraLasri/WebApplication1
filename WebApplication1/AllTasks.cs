0b73882460213a986cb6850dd8584aa21e3c8604
faigygit <37561388215@mby.co.il>
2023-11-07 17:11:17 +0200

    hello

--- WebApplication1/AllTasks.cs
+++ WebApplication1/AllTasks.cs
@@ -7,7 +7,7 @@
         new TaskModel(1, "fff","Yellow", DateTime.Now,Preferance.a),
         new TaskModel(2,"ggg","Orange", DateTime.Now,Preferance.a),
         new TaskModel(3, "fff","Green", DateTime.Now,Preferance.a),
-        new TaskModel(4, "uuu","Orange", DateTime.Now,Preferance.a)
+        new TaskModel(4, "uuu","Blue", DateTime.Now,Preferance.a)
         };
     }
 }
