a398ef4f6c4cf3a9e0c28b2d62ece8c63e465344
faigygit <37561388215@mby.co.il>
2023-11-07 17:07:52 +0200

    new tasks

--- WebApplication1/AllTasks.cs
+++ WebApplication1/AllTasks.cs
@@ -5,9 +5,9 @@
         public static List<TaskModel> allTasks = new List<TaskModel>{
 
         new TaskModel(1, "fff","Orange", DateTime.Now,Preferance.a),
-        new TaskModel(2,"fff","Orange", DateTime.Now,Preferance.a),
-        new TaskModel(3, "fff","Orange", DateTime.Now,Preferance.a),
-        new TaskModel(4, "fff","Orange", DateTime.Now,Preferance.a)
+        new TaskModel(2,"ggg","Orange", DateTime.Now,Preferance.a),
+        new TaskModel(3, "fff","Green", DateTime.Now,Preferance.a),
+        new TaskModel(4, "uuu","Orange", DateTime.Now,Preferance.a)
         };
     }
 }
