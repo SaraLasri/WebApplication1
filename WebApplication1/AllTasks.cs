2428c6a163ba3a48945525d28b1199a3e3eb556b
SaraLasri <326206984@PC5-24>
2023-11-10 09:53:26 +0200

    dd

--- WebApplication1/AllTasks.cs
+++ WebApplication1/AllTasks.cs
@@ -4,7 +4,7 @@
     {
         public static List<TaskModel> allTasks = new List<TaskModel>{
 
-        new TaskModel(1, "fff","Orange", DateTime.Now,Preferance.a),
+        new TaskModel(1, "sraarrr","Orange", DateTime.Now,Preferance.a),
         new TaskModel(2,"fff","Orange", DateTime.Now,Preferance.a),
         new TaskModel(3, "fff","Orange", DateTime.Now,Preferance.a),
         new TaskModel(4, "fff","Orange", DateTime.Now,Preferance.a),
