a02a733cd127434ce7314e61d0736fc2d2b57b9f
faigygit <37561388215@mby.co.il>
2023-11-10 09:26:51 +0200

    new tassks

--- WebApplication1/AllTasks.cs
+++ WebApplication1/AllTasks.cs
@@ -8,10 +8,13 @@
         new TaskModel(2,"sss","Orange", DateTime.Now,Preferance.a),
         new TaskModel(3, "ggg","Orange", DateTime.Now,Preferance.a),
         new TaskModel(4, "yyy","Orange", DateTime.Now,Preferance.a),
-        new TaskModel(1, "ppp","purple", DateTime.Now,Preferance.a),
-        new TaskModel(2,"lll","yellow", DateTime.Now,Preferance.a),
-        new TaskModel(3, "kkk","pink", DateTime.Now,Preferance.a),
-        new TaskModel(4, "mmm","red", DateTime.Now,Preferance.a)
+        new TaskModel(5, "ppp","purple", DateTime.Now,Preferance.a),
+        new TaskModel(6,"lll","yellow", DateTime.Now,Preferance.a),
+        new TaskModel(7, "kkk","pink", DateTime.Now,Preferance.a),
+        new TaskModel(8, "mmm","red", DateTime.Now,Preferance.a),
+        new TaskModel(9,"lll","yellow", DateTime.Now,Preferance.a),
+        new TaskModel(10, "kkk","pink", DateTime.Now,Preferance.a),
+        new TaskModel(11, "mmm","red", DateTime.Now,Preferance.a)
         };
     }
 }
